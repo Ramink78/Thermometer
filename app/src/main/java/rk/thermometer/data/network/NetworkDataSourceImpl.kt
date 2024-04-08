0f5055662526d2ddbabca11eda382b853b175ff3
Ramin Karimi <raminkarimi1378@gmail.com>
2024-04-08 10:45:13 +0330

    read last temperature and humidity from remote database

--- app/src/main/java/rk/thermometer/data/network/NetworkDataSourceImpl.kt
+++ app/src/main/java/rk/thermometer/data/network/NetworkDataSourceImpl.kt
@@ -1,8 +1,9 @@
 package rk.thermometer.data.network
 
-import io.github.jan.supabase.realtime.PostgresAction
+import io.github.jan.supabase.annotations.SupabaseExperimental
+import io.github.jan.supabase.realtime.PrimaryKey
 import io.github.jan.supabase.realtime.channel
-import io.github.jan.supabase.realtime.postgresChangeFlow
+import io.github.jan.supabase.realtime.postgresListDataFlow
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import rk.thermometer.data.network.models.NetworkHumidity
@@ -12,25 +13,26 @@ import rk.thermometer.data.repository.NetworkDataSource
 class NetworkDataSourceImpl : NetworkDataSource {
     private val temperatureChannel = supabase.channel("temperatureChannel")
     private val humidityChannel = supabase.channel("humidityChannel")
+
+    @OptIn(SupabaseExperimental::class)
     override suspend fun getTemperatureFlow(): Flow<NetworkTemperature> {
         val temperatureChangeFlow =
-            temperatureChannel.postgresChangeFlow<PostgresAction.Insert>(schema = "public") {
-                table = temperatureTable
-            }.map {
-                NetworkTemperature(it.record["celcius"].toString())
-            }
+            temperatureChannel.postgresListDataFlow<NetworkTemperature>(
+                table = "temperature",
+                primaryKey = PrimaryKey("id") { it.id.toString() }
+            ).map { it.last() }
+
         temperatureChannel.subscribe(true)
         return temperatureChangeFlow
 
     }
 
+    @OptIn(SupabaseExperimental::class)
     override suspend fun getHumidityFlow(): Flow<NetworkHumidity> {
         val humidityChangeFlow =
-            humidityChannel.postgresChangeFlow<PostgresAction.Insert>(schema = "public") {
-                table = humidityTable
-            }.map {
-                NetworkHumidity(it.record["percent"].toString())
-            }
+            humidityChannel.postgresListDataFlow<NetworkHumidity>(
+                table = "humidity",
+                primaryKey = PrimaryKey("id") { it.id.toString() }).map { it.last() }
         humidityChannel.subscribe(true)
         return humidityChangeFlow
     }
--- app/src/main/java/rk/thermometer/data/network/models/Models.kt
+++ app/src/main/java/rk/thermometer/data/network/models/Models.kt
@@ -1,19 +1,32 @@
 package rk.thermometer.data.network.models
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import rk.thermometer.data.local.LocalHumidity
 import rk.thermometer.data.local.LocalTemperature
 
 @Serializable
 data class NetworkHumidity(
-    val value: String
+    @SerialName("id")
+    val id:Int,
+    @SerialName("created_at")
+    val createdAt: String,
+    @SerialName("percent")
+    val percent: Float
 )
 
 @Serializable
 data class NetworkTemperature(
-    val value: String
+    @SerialName("id")
+    val id: Int,
+    @SerialName("created_at")
+    val createdAt: String,
+    @SerialName("celcius")
+    val celcius: Float,
+    @SerialName("fahrenheit")
+    val fahrenheit: Float
 )
 fun NetworkHumidity.toLocal()=
-    LocalHumidity(value=value, lastUpdate = "")
+    LocalHumidity(value=percent.toString(), lastUpdate = "")
 fun NetworkTemperature.toLocal()=
-    LocalTemperature(value = value, lastUpdate = "")
+    LocalTemperature(value = celcius.toString(), lastUpdate = "")
